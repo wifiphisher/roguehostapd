9383054f1532bb601cd6f1b7a3c533d10d5a729b
Anakin <anakin1028@gmail.com>
2017-05-24 14:59:59 +0800

    Add setup.py to build the hostapd shared library

---
+++ setup.py
@@ -0,0 +1,42 @@
+"""
+Module for setup hostapd shared library
+"""
+
+import os
+from distutils.command.build import build
+from subprocess import call
+from setuptools import setup
+
+BASEPATH = os.path.dirname(os.path.abspath(__file__))
+HOSTAPD_BUILD_PATH = os.path.join(BASEPATH, 'hostapd-2.6/hostapd')
+
+
+class HostapdBuild(build):
+    """
+    Class for build the shared library of hostapd
+    """
+
+    def run(self):
+
+        build.run(self)
+        make_cmd = ['make', 'hostapd_lib']
+        cp_cmd = ['cp', 'defconfig', '.config']
+
+        def compile_hostapd():
+            """
+            Compile the shared library of hostapd
+            """
+            call(cp_cmd, cwd=HOSTAPD_BUILD_PATH)
+            call(make_cmd, cwd=HOSTAPD_BUILD_PATH)
+
+        self.execute(compile_hostapd, [], 'Compiling hostapd shared library')
+
+setup(
+    name='hostapd_binder',
+    version='1.0',
+    description='Hostapd wrapper for hostapd',
+    author='Anakin',
+    cmdclass={
+        'build': HostapdBuild,
+        }
+)
