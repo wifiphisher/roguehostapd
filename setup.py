c2fe468e7e93dc4d1fc88c3ec3d89cb54fc62bab
Brian Smith <brian.smith@riseup.net>
2018-07-19 09:16:48 -0400

    more fixes

--- setup.py
+++ setup.py
@@ -10,16 +10,17 @@ except ImportError:
     from distutils.core import setup
     from distutils.command.install import install
 from distutils.command.build_ext import build_ext
+from contextlib import redirect_stdout
+from os import devnull
 import roguehostapd.buildutil.buildcommon as buildcommon
 import roguehostapd.buildutil.buildexception as buildexception
 from roguehostapd.config.hostapdconfig import WHITE, RED
 
 # define project information
 NAME = 'roguehostapd'
-PACKAGES = ['roguehostapd',
-            'examples',
-            'roguehostapd.config',
-            'roguehostapd.buildutil']
+PACKAGES = [
+    'roguehostapd', 'examples', 'roguehostapd.config', 'roguehostapd.buildutil'
+]
 PACKAGE_DIR = {'roguehostapd': 'roguehostapd'}
 PACKAGE_DATA = {'roguehostapd': ['config/hostapd.conf', 'config/config.ini']}
 VERSION = '1.1.2'
@@ -29,7 +30,7 @@ AUTHOR = 'Anakin'
 
 try:
     EXT_MODULE = buildcommon.get_extension_module()
-    with buildcommon.nostdout():
+    with redirect_stdout(devnull):
         setup(
             name=NAME,
             packages=PACKAGES,
@@ -39,29 +40,18 @@ try:
             description=DESCRIPTION,
             url=URL,
             author=AUTHOR,
-            install_requires=["future", "configparser"],
+            install_requires=[],
             zip_safe=False,
-            cmdclass={'build_ext': build_ext,
-                      'install': install},
-            ext_modules=EXT_MODULE
-        )
+            cmdclass={
+                'build_ext': build_ext,
+                'install': install
+            },
+            ext_modules=EXT_MODULE)
 except buildexception.SharedLibMissError as exobj:
-    print ("[" + RED + "!" + WHITE + "] " +
-           ("The development package for " + exobj.libname +
-            " is missing. Please download it and restart the compilation."
-            "If you are on Debian-based system: \'apt-get install{}\'.".format(
-                "".join(" " + package for package in exobj.packages))))
-    with buildcommon.nostdout():
-        setup(
-            name=NAME,
-            packages=PACKAGES,
-            package_dir=PACKAGE_DIR,
-            package_data=PACKAGE_DATA,
-            version=VERSION,
-            description=DESCRIPTION,
-            zip_safe=False,
-            url=URL,
-            author=AUTHOR,
-        )
+    print("[" + RED + "!" + WHITE + "] " +
+          ("The development package for " + exobj.libname +
+           " is missing. Please download it and restart the compilation."
+           "If you are on Debian-based system: \'apt-get install{}\'.".format(
+               "".join(" " + package for package in exobj.packages))))
 finally:
     shutil.rmtree('tmp')
